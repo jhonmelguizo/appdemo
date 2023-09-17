3b6eebedfefd19e4a4c92171ef1dd427566e410e
Jhon Arley <jhon.melguizo@gmail.com>
2023-09-17 13:39:35 -0500

    Ajustes
    Ajustes

--- build.bash
+++ build.bash
@@ -4,7 +4,7 @@
 javac -cp .:lib/* src/*.java -d build
 
 # Empaquetar la aplicación en un archivo JAR
-jar -cf myapp.jar -C build/ .
+jar -cf app.jar -C build/ .
 
 # Mover el JAR a la ubicación deseada
-mv myapp.jar /usr/share/
+mv app.jar /usr/share/
